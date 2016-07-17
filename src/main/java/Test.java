c2f0071228129999aaab786279d62dc66d66ab79
yafei.cyf <yafei.cyf@cainiao.com>
2016-07-17 16:44:29 +0800

    project init...

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+import java.util.Optional;
+
+/**
+ * Created by zyf on 16/7/15.
+ */
+public class Test {
+    public static void main(String[] args) {
+        Optional<Integer> num = Optional.ofNullable(1);
+        System.out.println(num.get());
+    }
+}
